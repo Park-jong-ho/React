0716ad29ea26d89c33dced6ab047dcdd3d85f75b
jongho <whjk1104@gmail.com>
2024-11-20 14:36:23 +0900

    ✨ React SCSS(2/3)
    React SCSS(2/3)

--- scss.11.20/src/App.jsx
+++ scss.11.20/src/App.jsx
@@ -6,11 +6,28 @@ function App() {
       <div className='test'>Hello</div>
       <h2>SCSS 변수 연습</h2>
       <ul>
-        <li>안녕하세요</li>
-        <li>반갑습니다</li>
-        <li>SCSS를</li>
-        <li>공부해봅시다</li>
+        <li>안녕하세요
+          <p>p 요소입니다.</p>
+        </li>
+        <li>반갑습니다
+          <div>div 요소입니다.</div>
+        </li>
+        <li>SCSS를<br />
+          <span>span 요소입니다.
+            <br />
+            <h3>h3 요소입니다.</h3>
+          </span>
+        </li>
+        <li>공부해봅시다
+          <h2>h2 요소입니다.</h2>
+        </li>
       </ul>
+      <section>
+        <article>SCSS문법에는</article>
+        <article>변수 사용과</article>
+        <article>중첩 문법과</article>
+        <article>믹스인 등이 있습니다.</article>
+      </section>
     </div>
   );
 }
