69e051cbafc90b5cae7db8c44dd329d858cf4dfd
jongho <whjk1104@gmail.com>
2024-11-22 16:52:28 +0900

    ✨ React TailwindCSS(2/2)
    React TailwindCSS(2/2)

--- 11.22.TailwindCSS/src/App.jsx
+++ 11.22.TailwindCSS/src/App.jsx
@@ -1,15 +1,23 @@
-import './App.css'
+import { contents } from "./assets/data/contents";
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Tab from "./components/Tab";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <div className='flex justify-center items-center h-screen'>
-          {/* []를 사용하여 직접 원하는 값을 넣을 수 있다. */}
-        <div className='text-5xl text-[#367684]'>안녕하세요</div>
-      </div>
-    </>
-  )
+    <div className="grid grid-cols-[80px_1fr] grid-rows-[80px_40px_1fr] w-screen h-screen">
+      <Header />
+      <Nav />
+      <Tab />
+      <main className="w-full p-[20px] flex flex-wrap items-start">
+        {contents.map((el) => (
+          <Content key={el.id} content={el} />
+        ))}
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ 11.22.TailwindCSS/src/TailwindCSS.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen'>
+          {/* []를 사용하여 직접 원하는 값을 넣을 수 있다. */}
+        <div className='text-5xl text-[#367684]'>안녕하세요</div>
+      </div>
+    </>
+  )
+}
+
+export default App
