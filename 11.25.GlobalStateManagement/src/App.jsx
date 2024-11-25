6ca36d61f85507951ce227c47310d924fa87f2a3
jongho <whjk1104@gmail.com>
2024-11-25 15:38:17 +0900

    React 전역 상태 관리

--- 11.25.GlobalStateManagement/src/App.jsx
+++ 11.25.GlobalStateManagement/src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState } from "react"
 
 const Component = styled.div`
   font-weight: 700;
@@ -21,49 +22,50 @@ const Container = styled.div`
 `;
 
 export default function App() {
+  const [counter, setCounter] = useState(1)
   console.log("App");
   return (
     <Container>
       <Component>
         App
         <Container>
-          <Child1 />
-          <Child2 />
+          <Child1 counter={counter}/>
+          <Child2 counter={counter} setCounter={setCounter} />
         </Container>
       </Component>
     </Container>
   );
 }
 
-function Child1() {
+function Child1({ counter }) {
   console.log("Child1");
   return (
     <Component>
       Child1
       <Container>
-        <Child3 />
+        <Child3 counter={counter}/>
         <Child4 />
       </Container>
     </Component>
   );
 }
 
-function Child2() {
+function Child2({counter, setCounter}) {
   console.log("Child2");
   return (
     <Component>
       Child2
       <Container>
         <Child5 />
-        <Child6 />
+        <Child6 counter={counter} setCounter={setCounter} />
       </Container>
     </Component>
   );
 }
 
-function Child3() {
+function Child3({ counter }) {
   console.log("Child3");
-  return <Component>Child3 : 0</Component>;
+  return <Component>Child3 : {counter}</Component>;
 }
 
 function Child4() {
@@ -76,12 +78,12 @@ function Child5() {
   return <Component>Child5</Component>;
 }
 
-function Child6() {
+function Child6({counter, setCounter}) {
   console.log("Child6");
   return (
     <Component>
       Child6
-      <button onClick={() => {}}>+</button>
+      <button onClick={() => {setCounter(counter + 1)}}>+</button>
     </Component>
   );
 }
