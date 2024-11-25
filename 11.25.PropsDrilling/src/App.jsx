212353d63702a91ec91fe284619e9e91d5b4ae25
jongho <whjk1104@gmail.com>
2024-11-25 15:51:34 +0900

    ✨ React Props Drilling Setting
    React Props Drilling Setting

--- 11.25.PropsDrilling/src/App.jsx
+++ 11.25.PropsDrilling/src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css'
+import { useState } from 'react'
+import './App.scss'
+import data from './assets/data'
+import Header from './components/Header'
+import Menu from './components/Menu'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './components/Cart'
 
 function App() {
-  
+  const [ menu, setMenu ] = useState(data.menu)
+  const [ cart, setCart ] = useState([])
+
+  console.log(cart)
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path='/' element={<Menu />}/>
+          <Route path='/cart' element={<Cart />}/>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
