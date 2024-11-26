145c1aa5fcfa1699aa82c22eb6825a39ea8d2035
jongho <whjk1104@gmail.com>
2024-11-26 11:33:21 +0900

    ✨ React Context API Setting (전역상태관리)
    React Context API Setting (전역상태관리)

--- 11.26.ContextAPI/11.26.ContextAPI/src/main.jsx
+++ 11.26.ContextAPI/11.26.ContextAPI/src/main.jsx
@@ -1,10 +1,5 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { CounterProvider } from './context/counterContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <CounterProvider>
-    <App />
-  </CounterProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
