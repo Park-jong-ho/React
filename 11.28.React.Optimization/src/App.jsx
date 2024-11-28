f8d145edf205fbc91e39f17a3d130d488738d27f
jongho <whjk1104@gmail.com>
2024-11-28 14:14:58 +0900

    🎉 Make React.Optimization File
    Make React.Optimization File

---
+++ 11.28.React.Optimization/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div>hello</div>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
+/*
+useMemo() : 함수를 호출 결과를 저장
+
+useCallback() : 함수를 저장
+memoryUsage() : 컴포넌트를 저장
+*/
